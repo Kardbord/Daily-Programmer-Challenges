0d7299fc88e535305c4d6879fe37aeb6713fd8a2
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2017-05-12 11:57:00 -0600

    implemented init_inputs

--- Daily_Prog#312/main.cpp
+++ Daily_Prog#312/main.cpp
@@ -1,10 +1,31 @@
 #include <iostream>
+#include <vector>
+#include <fstream>
 
+// Function Prototypes
+bool init_inputs(int const & argc, char *argv[], std::vector<int> & inputs);
+
+// MAIN
 int main(int argc, char *argv[]) {
-    if (argc > 2) {
-        std::cerr << "Too many program arguments provided -- expected 1" << std::endl;
-        return EXIT_FAILURE;
-    }
+    std::vector<int> inputs;
+    if(!init_inputs(argc, argv, inputs)) return EXIT_FAILURE;
 
     return EXIT_SUCCESS;
+} // END MAIN
+
+bool init_inputs(int const & argc, char *argv[], std::vector<int> & inputs) {
+    if (argc > 2) {
+        std::cerr << "Too many command line arguments provided -- expected 1" << std::endl;
+        return false;
+    }
+
+    std::ifstream fin(argv[1]);
+    if (!fin) {
+        std::cerr << "Error opening " << argv[1] << " , terminating program" << std::endl;
+        return false;
+    }
+
+    // read in file
+    for (int i = 0; fin >> inputs[i]; ++i);
+    return true;
 }
