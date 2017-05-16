759480ff7edb1b6ad9722a093fe1985f539c248e
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2017-05-15 23:54:26 -0600

    Got compute_next_largest working for all of my test cases

--- Daily_Prog#312/main.cpp
+++ Daily_Prog#312/main.cpp
@@ -74,7 +74,7 @@ std::string compute_next_largest(std::string const &input) {
      * See http://stackoverflow.com/questions/11255684/why-c-does-not-support-multiple-initializers-in-for-loop
      */
     for (struct { int index; bool swapped, out_of_bounds; } loopVars = {(int) nextLargest.size() - 1, false, false};
-         !loopVars.swapped; --loopVars.index) {
+         !loopVars.swapped && !loopVars.out_of_bounds; --loopVars.index) {
         if (loopVars.index == 1) loopVars.out_of_bounds = true;
         for (int j = 1; !loopVars.swapped && j <= loopVars.index; ++j) {
             // If j == loopVars.index, then we are out of digits to check and need to stop looping
