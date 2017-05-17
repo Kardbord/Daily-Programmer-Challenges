2c2be974ad163b4b598f2e64d355f9099391e3e0
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2017-05-17 11:52:43 -0600

    Set up to begin work on challenge 277E

---
+++ Daily_Prog#277E/src/Simplifier.java
@@ -0,0 +1,12 @@
+/**
+ * Daily_Prog#277E
+ * Created on 5/17/2017
+ *
+ * TODO Add a short description of the class here
+ *
+ * @author Tanner Kvarfordt
+ * @version 1.0
+ */
+public class Simplifier {
+
+}
