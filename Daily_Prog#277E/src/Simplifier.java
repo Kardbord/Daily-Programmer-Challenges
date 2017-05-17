bcdd403c80cb80317102092ff45f1e3f54858988
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2017-05-17 13:47:56 -0600

    Implemented computeGCF, needs tested

--- Daily_Prog#277E/src/Simplifier.java
+++ Daily_Prog#277E/src/Simplifier.java
@@ -20,4 +20,23 @@ public class Simplifier {
     public static void simplify(Integer numerator, Integer denominator) {
 
     }
+
+    /**
+     * Calculates the greatest common factor of a and the b using the Euclidean algorithm
+     * See https://www.khanacademy.org/computing/computer-science/cryptography/modarithmetic/a/the-euclidean-algorithm
+     *
+     * @param a is the first Integer
+     * @param b is the second Integer
+     * @return the greatest common factor of the a and b
+     */
+    public static Integer computeGCF(Integer a, Integer b) {
+        Integer c = Math.abs(a);
+        Integer d = Math.abs(b);
+
+        if (c == 0) return d;
+        if (d == 0) return c;
+        if (c.equals(d)) return c;
+        if (c < d) return computeGCF(d, c);
+        return computeGCF(d, c % d);
+    }
 }
