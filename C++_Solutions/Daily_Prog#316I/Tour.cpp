722a38affb5205ac27e55d62972cb361b46cb96e
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2017-05-24 13:41:27 -0600

    Added ToursDB and Tour classes

---
+++ C++_Solutions/Daily_Prog#316I/Tour.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Tanner on 5/24/2017.
+//
+
+#include "Tour.h"
---
+++ C++_Solutions/Daily_Prog#316I/ToursDB.h
@@ -0,0 +1,14 @@
+//
+// Created by Tanner on 5/24/2017.
+//
+
+#ifndef DAILY_PROG_316I_TOURSDB_H
+#define DAILY_PROG_316I_TOURSDB_H
+
+
+class ToursDB {
+    
+};
+
+
+#endif //DAILY_PROG_316I_TOURSDB_H
