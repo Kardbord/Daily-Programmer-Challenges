aacb2f05dfcbd2787aa18152f5e99b1f62c160cf
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2017-05-24 13:51:15 -0600

    Implemented Tour class

--- C++_Solutions/Daily_Prog#316I/Tour.cpp
+++ C++_Solutions/Daily_Prog#316I/Tour.cpp
@@ -3,3 +3,6 @@
 //
 
 #include "Tour.h"
+
+Tour::Tour(std::string const &id, std::string const &name, unsigned int const &price) : m_id(id), m_name(name),
+                                                                                  m_price(price) {}
--- C++_Solutions/Daily_Prog#316I/Tour.h
+++ C++_Solutions/Daily_Prog#316I/Tour.h
@@ -5,12 +5,24 @@
 #ifndef DAILY_PROG_316I_TOUR_H
 #define DAILY_PROG_316I_TOUR_H
 
+#include <string>
 
 class Tour {
 public:
+    Tour(std::string const &id, std::string const &name, unsigned int const &price);
+
+    std::string getID() const { return m_id; }
+
+    std::string getName() const { return m_name; }
+
+    unsigned int getPrice() const { return m_price; }
 
 private:
+    std::string m_id;
 
+    std::string m_name;
+
+    unsigned int m_price;
 };
 
 
