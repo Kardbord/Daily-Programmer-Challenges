cee9ee8becc28dfdb4724022ffeab5a03d7bc8e8
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2017-05-24 13:35:17 -0600

    Added Cart and Promotions classes

---
+++ C++_Solutions/Daily_Prog#316I/Cart.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Tanner on 5/24/2017.
+//
+
+#include "Cart.h"
---
+++ C++_Solutions/Daily_Prog#316I/Cart.h
@@ -0,0 +1,14 @@
+//
+// Created by Tanner on 5/24/2017.
+//
+
+#ifndef DAILY_PROG_316I_CART_H
+#define DAILY_PROG_316I_CART_H
+
+
+class Cart {
+
+};
+
+
+#endif //DAILY_PROG_316I_CART_H
