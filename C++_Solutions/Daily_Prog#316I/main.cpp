7ffaef92d5a2d05819a4aa9901ff37f77e725cc0
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2017-06-01 13:58:09 -0600

    Added todos

--- C++_Solutions/Daily_Prog#316I/main.cpp
+++ C++_Solutions/Daily_Prog#316I/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include "ToursDB.h"
 
+// TODO: provide database inputs via cmd line args
+// TODO: take tour id's via user input when building a cart
+
 int main() {
     ToursDB toursDB;
 
