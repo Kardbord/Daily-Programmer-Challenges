a230f4f04b530c94b6a30c15112f6cd83157c91f
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2017-06-01 15:36:35 -0600

    Added Utils class with a parseString function

---
+++ C++_Solutions/Daily_Prog#316I/Utils.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Tanner on 6/1/2017.
+//
+
+#include <stdexcept>
+#include "Utils.h"
+
+std::vector<std::string> Utils::parseString(std::string s, std::string const &delim, int const expected_tokens) {
+    std::vector<std::string> tokens;
+    size_t pos = 0;
+    while ((pos = s.find(delim)) != std::string::npos) {
+        tokens.push_back(s.substr(0, pos));
+        s.erase(0, pos + delim.length());
+    }
+
+    if (tokens.size() != expected_tokens) {
+        throw std::invalid_argument("expected_tokens did not match the number of tokens parsed");
+    }
+
+    return tokens;
+}
---
+++ C++_Solutions/Daily_Prog#316I/Utils.h
@@ -0,0 +1,28 @@
+//
+// Created by Tanner on 6/1/2017.
+//
+
+#ifndef DAILY_PROG_316I_UTILS_H
+#define DAILY_PROG_316I_UTILS_H
+
+#include <string>
+#include <vector>
+
+class Utils {
+public:
+
+    /**
+     * Adapted from stack overflow: https://stackoverflow.com/questions/14265581/parse-split-a-string-in-c-using-string-delimiter-standard-c
+     *
+     * Parses a string based on a provided delimiter, and returns a vector containing the parsed tokens
+     *
+     * @param s is the string to be parsed
+     * @param delim is the delimiter by which %s will be parsed
+     * @return a vector containing the parsed tokens, minus the delimiter
+     */
+    static std::vector<std::string> parseString(std::string s, std::string const &delim, int const expected_tokens);
+
+};
+
+
+#endif //DAILY_PROG_316I_UTILS_H
