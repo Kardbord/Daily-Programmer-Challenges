3a0ffc1c2927ef06d1dafdefd332295177af0a17
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2017-06-08 11:57:14 -0600

    Removed PromoDB constructor that used input file -- too much of a headache

--- C++_Solutions/Daily_Prog#316I/PromoDB.h
+++ C++_Solutions/Daily_Prog#316I/PromoDB.h
@@ -24,19 +24,6 @@ public:
      */
     PromoDB(ToursDB const &toursDB, std::vector<std::pair<std::string, unsigned int>> freebies, unsigned int discount);
 
-    /**
-     *
-     * @param toursDB is a ToursDB object. It is used to build a set of valid tour IDs that Promotions can apply to.
-     * @param fin is the input file through which promotions will be created and added to the PromoDB
-     *
-     * The input file must be formatted as follows:
-     *     - One Promotion per line
-     *     - Each line follows the format:
-     *         <promo_id> <number of tour_id's (keys) to expect> <appropriate number of tour id's> <appropriate number of amt_req values to go with the tour_ids>
-     */
-    PromoDB(ToursDB const &toursDB, std::ifstream &fin, std::vector<std::pair<std::string, unsigned int>> freebies,
-            unsigned int discount);
-
 };
 
 
