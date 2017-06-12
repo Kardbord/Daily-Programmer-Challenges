6bc716ce01b9f78b8570dec24af9c708bcb4afa3
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2017-06-12 11:07:39 -0600

    Began work on Cart class. Implemented constructor and addTour.

--- C++_Solutions/Daily_Prog#316I/Cart.cpp
+++ C++_Solutions/Daily_Prog#316I/Cart.cpp
@@ -3,3 +3,13 @@
 //
 
 #include "Cart.h"
+
+Cart::Cart(PromoDB const &promoDB, ToursDB const &toursDB) : m_promoDB(promoDB), m_tourDB(toursDB), m_items() {}
+
+bool Cart::addTour(Tour const &tour) {
+    if (m_tourDB.find(tour.getID()) != m_tourDB.end()) {
+        m_items.push_back(tour);
+        return true;
+    } else return false;
+}
+
--- C++_Solutions/Daily_Prog#316I/Cart.h
+++ C++_Solutions/Daily_Prog#316I/Cart.h
@@ -5,9 +5,30 @@
 #ifndef DAILY_PROG_316I_CART_H
 #define DAILY_PROG_316I_CART_H
 
+#include "PromoDB.h"
+#include "ToursDB.h"
+
 // TODO: start here next time. Begin implementing Cart. Update the Tour and ToursDB classes as needed, then work on main
 class Cart {
+public:
 
+    Cart(PromoDB const &promoDB, ToursDB const &tourDB);
+
+    /**
+     *
+     * @param tour is the Tour to be added to m_items
+     * @return true if the Tour is succesfully added, false otherwise
+     */
+    bool addTour(Tour const &tour);
+
+
+
+private:
+    std::vector<Tour> m_items;
+
+    PromoDB m_promoDB;
+
+    ToursDB m_tourDB;
 };
 
 
