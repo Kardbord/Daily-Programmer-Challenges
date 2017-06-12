7133fd09e813bd34ed483b42c50de8aba6e955c9
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2017-06-12 12:03:21 -0600

    Cleared main.cpp of test cases for a fresh slate

--- C++_Solutions/Daily_Prog#316I/main.cpp
+++ C++_Solutions/Daily_Prog#316I/main.cpp
@@ -1,13 +1,7 @@
-#include <iostream>
-#include "ToursDB.h"
-
-// TODO: provide database inputs via cmd line args
-// TODO: take tour id's via user input when building a cart
+/*
+ * Created by Tanner
+ */
 
 int main() {
-    ToursDB toursDB;
 
-    Tour tour("OH", "asdf", 200);
-
-    toursDB.addTour(tour);
 }
