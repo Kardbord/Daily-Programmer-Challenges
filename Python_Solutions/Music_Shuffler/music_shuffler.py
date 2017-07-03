7bd888434d53fd852ab4e3f9584da0ff63c9ba53
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2017-07-03 12:57:04 -0600

    Added Track class

--- Python_Solutions/Music_Shuffler/music_shuffler.py
+++ Python_Solutions/Music_Shuffler/music_shuffler.py
@@ -4,3 +4,9 @@
 # A Playlist class with an array of Tracks, and a shuffle function
 # Other Stuff:
 # Take inputs from a file if possible to build a Track library/Playlist
+
+
+class Track(object):
+    def __init__(self, artist, album, pk):
+        self.artist = artist
+        self.album = album
